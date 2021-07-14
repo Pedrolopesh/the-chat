1494be852be603acd598aff8287a60442c002839
Pedrolopesh <47143677+Pedrolopesh@users.noreply.github.com>
2021-07-13 23:26:32 -0300

    still fix push notifications

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,25 +1,45 @@
-function receivePushNotification(event) {
-  console.log("[Service Worker] Push Received.");
+// function receivePushNotification(event) {
+//   console.log("[Service Worker] Push Received.");
 
-  //const { image, tag, url, title, text } = event.data.json();
-  const notificationText = event.data.text();
-  const title = "Link Leito:"
+//   //const { image, tag, url, title, text } = event.data.json();
+//   const notificationText = event.data.text();
+//   const title = "Link Leito:"
 
+//   const options = {
+//     //data: url,
+//     //data: "something you want to send within the notification, such an URL to open",
+//     body: notificationText,
+//     icon: "./logo-img.png",
+//     vibrate: [200, 100, 200],
+//     //tag: tag,
+//     //image: image,
+//     badge: "https://res.cloudinary.com/dxblalpv2/image/upload/v1588194718/favicon_cdghd2.ico",
+//     //actions: [{ action: "Detail", title: "View", icon: "https://via.placeholder.com/128/ff0000" }]
+//   };
+//   //call the method showNotification to show the notification
+//   event.waitUntil(self.registration.showNotification(title, options));
+// }
+self.addEventListener("push", (event) => {
   const options = {
-    //data: url,
-    //data: "something you want to send within the notification, such an URL to open",
-    body: notificationText,
-    icon: "./logo-img.png",
+    body: 'HELLO PUSH NOTIFICATIONS',
+    icon: "./the-chat-logo.jpg",
     vibrate: [200, 100, 200],
+    data: {
+      dateOfArrival: Date.now(),
+      primaryKey: '2'
+    },
     //tag: tag,
     //image: image,
-    badge: "https://res.cloudinary.com/dxblalpv2/image/upload/v1588194718/favicon_cdghd2.ico",
-    //actions: [{ action: "Detail", title: "View", icon: "https://via.placeholder.com/128/ff0000" }]
+    actions: [
+        {
+          action: "explore",
+          title: "Explorethis new world",
+          icon: "./the-chat-logo-512x512.png"
+        }
+    ]
   };
-  //call the method showNotification to show the notification
-  event.waitUntil(self.registration.showNotification(title, options));
-}
-self.addEventListener("push", receivePushNotification);
+  event.waitUntil(self.registration.showNotification('HELLO WORD!', options));
+});
 self.addEventListener('activate', function(event) {
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
